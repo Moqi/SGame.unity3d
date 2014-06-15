8f50a7e874d54ae7dc5d6c4f957ca29626d40bc8
woojinlee <starlikecrawling@gmail.com>
2014-06-15 21:07:15 +0900

    첫번째
    커밋

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+	public float angleSpeed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, angleSpeed*Time.deltaTime);
+	}
+}
